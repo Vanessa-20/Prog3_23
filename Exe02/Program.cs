cd5c996264b6c0a39cc2989707fc530301dfc8fc
Vanessa Perazzoli Rossetto <371360@unoesc.lan>
2023-03-06 22:10:11 -0300

    Exe02

---
+++ Exe02/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.WriteLine("Olá, seja bem-vindo");
+Console.WriteLine("Qual é o seu nome?");
+string name = Console.ReadLine();
+Console.WriteLine($"Olá {name} prazer em conhecê-lo.");
+
+// Declarar sem inicializar / primitivo
+string message1;
+// Declarar inicializado / abstrato
+String message2 = "Seu Nome Completo";
+// Declarar string vazia
+string message3 = String.Empty;
+string message4 = "";
+
+// Declaração com tipo implícito
+var message5 = "Mensagem aleatória";
+
+message1 = "Rogério Ceni;Pelé;The Rock;Roberto Carlso";
+var palavras = message1.Split(';');
+foreach(var word in palavras)
+{
+    //Variável de escopo local do laço de repetição foreach
+    int a = 10; 
+    Console.WriteLine(word);
+}
+
+//laço de repetição : var:contador,acumulador,sinalizador(flag)
