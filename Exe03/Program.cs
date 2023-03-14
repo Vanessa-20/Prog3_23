a0b4e963b2b14fe065b0711a2c193340a4e9a1a3
Vanessa Perazzoli Rossetto <371360@unoesc.lan>
2023-03-13 21:33:39 -0300

    Exe03

---
+++ Exe03/Program.cs
@@ -0,0 +1,17 @@
+﻿static void FormatsString(string stringToFormat)
+{
+    stringToFormat.Replace("Mundo", "Marte");
+}
+
+static string FormatReturningString(string stringToFormat)
+{
+    return stringToFormat.Replace("Mundo", "Marte");
+}
+
+var mensagem = "Olá Mundo!";
+//Invocando o método sem retorno
+FormatsString(mensagem);
+Console.WriteLine(mensagem);
+
+var outraMensagem = "Bom-dia Mundo";
+Console.WriteLine( FormatReturningString(outraMensagem) );
