f747c835983bf6ea318a44bef1f4087defddd47b
Vanessa Perazzoli Rossetto <371360@unoesc.lan>
2023-03-13 21:48:00 -0300

    aula string numeros e conversão

---
+++ Exe04/Program.cs
@@ -0,0 +1,22 @@
+﻿var myFloat = 10f;
+float meuFloat = 11;
+
+//Conversão de tipo numérico
+
+int myInteger = 100;
+meuFloat = (float)myInteger;
+
+decimal myDecimal = Convert.ToDecimal(myInteger);
+
+//força o mydouble
+var myDouble = 10d;
+//chama mydouble
+myDecimal = 10m;
+
+string meuNumeroTexto = Convert.ToString(myInteger);
+int qtdChars = meuNumeroTexto.Length;
+for(int i = 0; i < qtdChars; i++)
+{
+    Console.WriteLine( meuNumeroTexto[i] );
+
+}
